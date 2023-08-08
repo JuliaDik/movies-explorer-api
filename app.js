2e693d17850915db6f0184d847d7f01df4858da5
Julia Dikolenko <tsvetkova-julia-vl@yandex.ru>
2023-08-08 17:48:26 +0300

    feat: реализовать логирование

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGODB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -18,8 +19,14 @@ mongoose.connect(MONGODB_URL, {
 // собираем в req.body поток запросов в формате JSON
 app.use(bodyParser.json());
 
+// записываем все поступающие запросы в отдельный файл ("журнал запросов")
+app.use(requestLogger);
+
 // подключаем маршруты запросов
 app.use(router);
+
+// записываем все возникающие ошибки в отдельный файл ("журнал ошибок")
+app.use(errorLogger);
 
 // взаимодействуем с клиентом через порт 3000
 app.listen(PORT, () => {
